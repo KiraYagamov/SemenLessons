c20c385bf62b2194bd6d3c3484870dd74ff43123
KiraYagamov <105204832+KiraYagamov@users.noreply.github.com>
2024-02-20 16:31:11 +0300

    commit
    Hello world!

---
+++ ProjectSemen/src/Base/Main.java
@@ -0,0 +1,10 @@
+package Base;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = Integer.parseInt(scanner.nextLine());
+    }
+}
